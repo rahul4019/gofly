57fe8474ab8b3a88317a531ffec3604b55a8c7d3
rahul4019 <rahulkashyap4019@gmail.com>
2026-07-08 07:35:29 +0530

    Implement email sending logic in consumer worker
    Add SMTP integration to the email worker to process recipients by
    sending an automated message via localhost. Includes a short delay to
    manage delivery rate.

--- consumer.go
+++ consumer.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/smtp"
 	"sync"
+	"time"
 )
 
 func emailWorker(id int, ch chan Recipient, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for recipient := range ch {
-		fmt.Println(id, recipient)
+		smtpHost := "localhost"
+		smtpPort := "1025"
+
+		formattedMsg := fmt.Sprintf("To: %s\r\nSubject: Test Email\r\n\r\n%s\r\n", recipient.Email, "Just testing our email campaign.")
+		msg := []byte(formattedMsg)
+
+		fmt.Printf("Worker %d: Sending email to %s \n", id, recipient.Email)
+
+		err := smtp.SendMail(smtpHost+":"+smtpPort, nil, "rahul@gmail.com", []string{recipient.Email}, msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		time.Sleep(50 * time.Microsecond)
+
+		fmt.Printf("Worker %d: Sent email to %s \n", id, recipient.Email)
 	}
 }
